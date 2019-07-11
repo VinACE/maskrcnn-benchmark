8635ecc44bc5ed73721499cbe7ad4d93e0902601
vinace <vinsent.storage@gmail.com>
2019-07-11 12:54:17 +0530

    checking segmentation map also to training

--- maskrcnn_benchmark/data/datasets/micr.py
+++ maskrcnn_benchmark/data/datasets/micr.py
@@ -99,7 +99,7 @@ class MICRDataset(torchvision.datasets.coco.CocoDetection):
             if value['image_id'] == image_id:
                 bbcollection.append(value['bbox'])
                 bblabel.append(value['category_id'])
-                segcollection.append(value['segmentation'])
+                segcollection.append(value['segmentations'])
         return (bbcollection,bblabel,segcollection)
 
 
