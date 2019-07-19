0e1d1965972087e177fc62b2a199db2d0580ca7c
vinace <vinsent.storage@gmail.com>
2019-07-19 20:09:28 +0530

    checking for the label data

--- maskrcnn_benchmark/config/defaults.py
+++ maskrcnn_benchmark/config/defaults.py
@@ -414,7 +414,7 @@ _C.SOLVER.CHECKPOINT_PERIOD = 100
 # Number of images per batch
 # This is global, so if we have 8 GPUs and IMS_PER_BATCH = 16, each GPU will
 # see 2 images per batch
-_C.SOLVER.IMS_PER_BATCH = 1
+_C.SOLVER.IMS_PER_BATCH = 2
 
 # ---------------------------------------------------------------------------- #
 # Specific test options
