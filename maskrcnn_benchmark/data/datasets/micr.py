ce50dbeeb9c7012773481063dcd336aca07b41c2
vinace <vinsent.storage@gmail.com>
2019-07-22 13:01:13 +0530

    enabling mask

--- maskrcnn_benchmark/data/datasets/micr.py
+++ maskrcnn_benchmark/data/datasets/micr.py
@@ -119,6 +119,8 @@ class MICRDataset(torchvision.datasets.coco.CocoDetection):
         import pdb; pdb.set_trace()
         boxes = torch.as_tensor(boxes).reshape(-1, 4)  # guard against no boxes
         target = BoxList(boxes, img.size, mode="xywh").convert("xyxy")
+        print(target)
+        
         # classes = [obj["category_id"] for obj in anno]
         classes = label
         classes = [self.json_category_id_to_contiguous_id[c] for c in classes]
