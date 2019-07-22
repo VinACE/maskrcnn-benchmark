50232ba213f41b576a7ab07bedaea5ad6619651a
vinace <vinsent.storage@gmail.com>
2019-07-22 12:43:49 +0530

    enabling mask

--- maskrcnn_benchmark/data/datasets/micr.py
+++ maskrcnn_benchmark/data/datasets/micr.py
@@ -125,7 +125,7 @@ class MICRDataset(torchvision.datasets.coco.CocoDetection):
         target.add_field("labels", classes)
         # if anno and "segmentation" in anno[0]:
         # masks = [obj["segmentation"] for obj in anno]
-               
+        print(f"printing target {type(target)}")
         masks = segcollection
         masks = SegmentationMask(masks, list(img.size, mode='poly')        
         target.add_field("masks", masks)
