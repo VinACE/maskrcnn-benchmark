a513a4eb6d9c739e85f18ad717360af7cfc711f5
vinace <vinsent.storage@gmail.com>
2019-07-25 16:48:39 +0530

    adding mask to the training

--- maskrcnn_benchmark/structures/segmentation_mask.py
+++ maskrcnn_benchmark/structures/segmentation_mask.py
@@ -371,7 +371,7 @@ class PolygonList(object):
 
         """
         if isinstance(polygons, (list, tuple)):
-            import pdb;pdb.set_trace()
+            # import pdb;pdb.set_trace()
             if len(polygons) == 0:
                 polygons = [[[]]]
             if isinstance(polygons[0], (list, tuple)):
@@ -492,7 +492,7 @@ class SegmentationMask(object):
             size: (width, height)
             mode: 'poly', 'mask'. if mode is 'mask', convert mask of any format to binary mask
         """
-
+        import pdb; pdb.set_trace()
         assert isinstance(size, (list, tuple))
         assert len(size) == 2
         if isinstance(size[0], torch.Tensor):
