961380e7551bdccdd8d757ee884f244a538d31be
danushk97 <danushk97@.gmail.com>
2019-07-29 15:07:30 +0530

    commenting segmentation mask for training now

--- maskrcnn_benchmark/structures/image_list.py
+++ maskrcnn_benchmark/structures/image_list.py
@@ -52,7 +52,7 @@ def to_image_list(tensors, size_divisible=0):
         print(" Iam in instance of tensor******************$$$$$$$$$") 
         print(tensors)
         # max_size = tuple(max(s) for s in zip(*[img.shape for img in tensors]))
-        max_size = tuple(max(s) for s in zip(*[img.shape for img in tensors]))
+        max_size = tuple(max(s) for s in zip(*[img.size() for img in tensors]))
 
         # TODO Ideally, just remove this and let me model handle arbitrary
         # input sizs
