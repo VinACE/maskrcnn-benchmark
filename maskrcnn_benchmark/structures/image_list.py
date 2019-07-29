a52e101b45959e72ba6088766d68ade2cbc8bcf4
danushk97 <danushk97@.gmail.com>
2019-07-29 14:53:06 +0530

    commenting segmentation mask for training now

--- maskrcnn_benchmark/structures/image_list.py
+++ maskrcnn_benchmark/structures/image_list.py
@@ -63,6 +63,7 @@ def to_image_list(tensors, size_divisible=0):
 
             stride = size_divisible
             max_size = list(max_size)
+            import pdb();pdb.set_trace()
             max_size[1] = int(math.ceil(max_size[1] / stride) * stride)
             max_size[2] = int(math.ceil(max_size[2] / stride) * stride)
             max_size = tuple(max_size)
