fc619d2767701f34a0786714909b7a79516fe9d8
vinace <vinsent.storage@gmail.com>
2019-08-02 18:29:52 +0530

    checking prediction

--- maskrcnn_benchmark/modeling/roi_heads/mask_head/mask_head.py
+++ maskrcnn_benchmark/modeling/roi_heads/mask_head/mask_head.py
@@ -58,7 +58,7 @@ class ROIMaskHead(torch.nn.Module):
             losses (dict[Tensor]): During training, returns the losses for the
                 head. During testing, returns an empty dict.
         """
-
+        import pdb;pdb.set_trace()
         if self.training:
             # during training, only focus on positive boxes
             all_proposals = proposals
@@ -69,7 +69,7 @@ class ROIMaskHead(torch.nn.Module):
         else:
             x = self.feature_extractor(features, proposals)
         mask_logits = self.predictor(x)
-        import pdb;pdb.set_trace()
+        
         if not self.training:
             result = self.post_processor(mask_logits, proposals)
             return x, result, {}
